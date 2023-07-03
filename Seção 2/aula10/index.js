de80920dc09e4e43c66abe8bca81e6b628a0f511
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-07-03 14:10:59 -0300

    Adicionei uma pasta geral para as aulas

---
+++ Seção 2/aula10/index.js
@@ -0,0 +1,8 @@
+// Tipos de dados:
+// String, number, undefined, null, boolean
+
+const nome = 'Luiz'; // string
+const num = 10; // number
+let nomeAluno; // undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // nulo -> não aponta para local nenhum na memória
+const aprovado = false; // boolean = true, false (lógico)
---
+++ Seção 2/aula11/js/index.js
@@ -0,0 +1,3 @@
+let nome = prompt('Qual é o seu nome?')
+alert('Seja bem vindo, '+nome)
+confirm('Muito obrigado!')
