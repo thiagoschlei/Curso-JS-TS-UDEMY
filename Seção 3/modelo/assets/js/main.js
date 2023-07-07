d79b541141badafa8acbbe09aea20f645cdba61d
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-07-07 20:11:52 -0300

    Fiz algumas modificações de indentação

--- Seção 3/modelo/assets/js/main.js
+++ Seção 3/modelo/assets/js/main.js
@@ -4,16 +4,16 @@ function enviar() {
     let imc = (peso / (altura * altura));
 
     if (imc <= 18.5) {
-        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você está abaixo do peso.";
+        res.innerHTML = "Seu IMC é: " + imc.toFixed(2) + ". Você está abaixo do peso.";
     } else if (imc >= 18.5 && imc <= 24.9) {
-        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você está no peso ideal.";
+        res.innerHTML = "Seu IMC é: " + imc.toFixed(2) + ". Você está no peso ideal.";
     } else if (imc >= 24.9 && imc <= 29.9) {
-        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem sobrepeso.";
+        res.innerHTML = "Seu IMC é: " + imc.toFixed(2) + ". Você tem sobrepeso.";
     } else if (imc >= 29.9 && imc <= 34.9) {
-        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem obesidade grau 1.";
+        res.innerHTML = "Seu IMC é: " + imc.toFixed(2) + ". Você tem obesidade grau 1.";
     } else if (imc >= 34.9 && imc <= 39.9) {
-        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem obesidade grau 2.";
+        res.innerHTML = "Seu IMC é: " + imc.toFixed(2) + ". Você tem obesidade grau 2.";
     } else {
-        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem obesidade grau 3."
+        res.innerHTML = "Seu IMC é: " + imc.toFixed(2) + ". Você tem obesidade grau 3."
     }
-}
+}   
