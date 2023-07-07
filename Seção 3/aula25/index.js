2a24ed910df07d081eb41c4b3f4ab52f05d31800
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-07-07 11:15:26 -0300

    Copiei as pastas de modelo das aulas

--- Seção 3/aula25/index.js
+++ Seção 3/aula25/index.js
@@ -1,2 +1,11 @@
-const numero = 10
+const numero = 5
 
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5')
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O número está entre 6 e 8')
+} else if (numero >= 9 && numero <= 11) {
+    console.log('O número está entre 9 e 11')
+} else {
+    console.log('O número NÃO está entre 0 e 11')
+}
