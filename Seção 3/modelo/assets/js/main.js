3e645dedfdede5ee2d2adef4c226603157c5dd77
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-07-07 13:00:33 -0300

    Fiz parte do exercício de IMC

---
+++ Seção 3/modelo/assets/js/main.js
@@ -0,0 +1,22 @@
+function enviar() {
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    let imc = (peso / (altura * altura));
+
+    console.log(peso)
+    console.log(altura)
+    console.log(imc)
+    if (imc <= 18.5) {
+        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você está abaixo do peso.";
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você está no peso ideal.";
+    } else if (imc >= 24.9 && imc <= 29.9) {
+        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem sobrepeso.";
+    } else if (imc >= 29.9 && imc <= 34.9) {
+        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem obesidade grau 1.";
+    } else if (imc >= 34.9 && imc <= 39.9) {
+        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem obesidade grau 2.";
+    } else {
+        res.innerHTML = "Seu IMC é: "+imc.toFixed(2)+". Você tem obesidade grau 3."
+    }
+}
