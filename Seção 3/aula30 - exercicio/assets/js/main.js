2d6910d5c96ce10839ba03fee6e6a7a4c95d9fbc
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-07-17 23:18:17 -0300

    Resolvi o exercício da aula 30

---
+++ Seção 3/aula30 - exercicio/assets/js/main.js
@@ -0,0 +1,72 @@
+const data = new Date();
+const diaSemana = data.getDay();
+const diaMes = data.getDate();
+const mes = data.getMonth();
+const ano = data.getFullYear();
+const hora = data.getHours();
+const minuto = data.getMinutes();
+let diaSemanaTexto;
+let mesTexto;
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo'
+        break
+    case 1:
+        diaSemanaTexto = 'Segunda-feira'
+        break
+    case 2:
+        diaSemanaTexto = 'Terça-feira'
+        break
+    case 3:
+        diaSemanaTexto = 'Quarta-feira'
+        break
+    case 4:
+        diaSemanaTexto = 'Quinta-feira'
+        break
+    case 5:
+        diaSemanaTexto = 'Sexta-feira'
+    case 6:
+        diaSemanaTexto = 'Sábado'
+        break
+    default:
+        diaSemanaTexto = 'Dia inválido'
+}
+switch (mes) {
+    case 0:
+        mesTexto = 'Janeiro'
+        break
+    case 1:
+        mesTexto = 'Fevereiro'
+        break
+    case 2:
+        mesTexto = 'Março'
+        break
+    case 3:
+        mesTexto = 'Abril'
+        break
+    case 4:
+        mesTexto = 'Maio'
+        break
+    case 5:
+        mesTexto = 'Junho'
+    case 6:
+        mesTexto = 'Julho'
+        break
+    case 7:
+        mesTexto = 'Agosto'
+        break
+    case 8:
+        mesTexto = 'Setembro'
+        break
+    case 9:
+        mesTexto = 'Outubro'
+        break
+    case 10:
+        mesTexto = 'Novembro'
+        break
+    case 11:
+        mesTexto = 'Dezembro'
+}
+
+res.innerHTML = `${diaSemanaTexto}, ${diaMes} de ${mesTexto} de ${ano} ${hora}:${minuto}`
