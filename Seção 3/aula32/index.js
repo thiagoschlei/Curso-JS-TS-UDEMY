54c7d5d8e4b1f9169050134097be8ebad51ed685
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-07-24 07:10:36 -0300

    Adicionei o restante da aula 32

--- Seção 3/aula32/index.js
+++ Seção 3/aula32/index.js
@@ -7,3 +7,24 @@ const letras = [b, c, a];
 
 
 console.log(a, b, c)
+
+/* uma maneira de fazer
+const numeros = [2, 8, 7, 9, 6, 1]
+const primeiro = numeros[0]
+
+console.log(primeiro)*/
+
+// maneira mais eficaz
+const num = [2, 8, 7, 9, 6, 1]
+//                 a vírgula com espaço vazia nomeia a variável do local
+//                                 ... pegam todas as variáveis que não foram declaradas anteriormente
+const [primeiroNum, , terceiroNum, ...resto] = num
+
+console.log(primeiroNum, terceiroNum)
+console.log(resto)
+
+//                   0          1          2
+//                0  1  2    0  1  2    0  1  2
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+console.log(numeros[1][0])
