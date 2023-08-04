02d30964fde809f6a8bd14ee2c788e392b5ae4aa
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-08-04 06:33:06 -0300

    Adicionei o try, catch, finally na aula

--- Seção 3/aula45/index.js
+++ Seção 3/aula45/index.js
@@ -20,7 +20,7 @@ function retornaHora(data) {
     if (!data) {
         data = new Date();
     }
-    return data.toLocaleTimeString('pt-BR',{
+    return data.toLocaleTimeString('pt-BR', {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
@@ -28,5 +28,12 @@ function retornaHora(data) {
 
 }
 
-const hora = retornaHora();
-console.log(hora)
+try {
+    const data = new Date('01-01-1998 11:35:00')
+    const hora = retornaHora(data);
+    console.log(hora)
+} catch (e) {
+    // Tratar erro
+} finally {
+    console.log('Tenha um bom dia!')
+}
