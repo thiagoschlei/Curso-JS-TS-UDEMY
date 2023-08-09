99c574d4ab8613bf2fb7cc3c0ea645bc3b70b701
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-08-09 20:33:39 -0300

    Adicionei restante do exercício

--- Seção 3/aula47 - exercicio/assets/js/main.js
+++ Seção 3/aula47 - exercicio/assets/js/main.js
@@ -11,26 +11,29 @@ const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 let segundos = 0;
+let timer;
 
-function iniciaRelogio(){
-    const timer = setInterval(function(){
-
-    },1000)
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000)
 }
 
-iniciar.addEventListener('click', function () {
-    let data = new Date();
-
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false
-    });
-    relogio.innerHTML = `${data.toLocaleTimeString('pt-BR')}`
+iniciar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
 })
 
 pausar.addEventListener('click', function mostraHora() {
-    relogio.innerHTML = 'oi'
+    relogio.classList.add('pausado');
+    clearInterval(timer);
 });
 
 zerar.addEventListener('click', function mostraHora() {
-    relogio.innerHTML = '00:00:00'
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
 });
