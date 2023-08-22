aed7e510675c553f10cf0a49f575dbb37839b85d
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-08-22 19:23:17 -0300

    Adicionei parte do exercício da aula 48

--- Seção 3/aula48 - exercicio/assets/js/main.js
+++ Seção 3/aula48 - exercicio/assets/js/main.js
@@ -19,10 +19,12 @@ function limpaInput() {
     inputTarefa.focus();
 }
 
-function criaBotaoApagar(li){
+function criaBotaoApagar(li) {
     li.innerText += ' ';
     const botaoApagar = document.createElement('button');
     botaoApagar.innerText = 'Apagar Tarefa';
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'Apagar esta tarefa');
     li.appendChild(botaoApagar);
 }
 
@@ -32,9 +34,28 @@ function criaTarefa(textoInput) {
     tarefas.appendChild(li);
     limpaInput();
     criaBotaoApagar(li);
+    salvarTarefas();
 };
 
 btnTarefa.addEventListener('click', function () {
     if (!inputTarefa.value) return;
     criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+       let tarefaTexto = tarefa.innerText;
+       tarefaTexto = tarefaTexto.replace('Apagar','').trim();
+       listaDeTarefas.push(tarefaTexto);
+    }
+}
