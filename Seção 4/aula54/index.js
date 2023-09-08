367800f792b39376873629dc483e60cdb5d1aaba
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-09-08 20:59:56 -0300

    Adicionei aula sobre callbacks

---
+++ Seção 4/aula54/index.js
@@ -0,0 +1,32 @@
+function rand(min = 1000, max = 3000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
+
+function f1() {
+    setTimeout(function (callback) {
+        console.log('f1');
+        if (callback) callback();
+    }, rand());
+
+}
+
+function f2() {
+    setTimeout(function (callback) {
+        console.log('f2');
+        if (callback) callback();
+    }, rand());
+}
+
+function f3() {
+    setTimeout(function (callback) {
+        console.log('f3');
+        if (callback) callback();
+    }, rand());
+}
+
+f1();
+f2();
+f3();
+
+
