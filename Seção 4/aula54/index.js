85c9b894358e158b02c03e5e6fe2683cbd9b983d
Thiago Schlei <thiago.indaialsm@hotmail.com>
2023-10-31 21:13:45 -0300

    Update index.js

--- Seção 4/aula54/index.js
+++ Seção 4/aula54/index.js
@@ -3,30 +3,38 @@ function rand(min = 1000, max = 3000) {
     return Math.floor(num);
 }
 
-function f1() {
-    setTimeout(function (callback) {
+function f1(callback) {
+    setTimeout(function () {
         console.log('f1');
         if (callback) callback();
     }, rand());
 
 }
 
-function f2() {
-    setTimeout(function (callback) {
+function f2(callback) {
+    setTimeout(function () {
         console.log('f2');
         if (callback) callback();
     }, rand());
 }
 
-function f3() {
-    setTimeout(function (callback) {
+function f3(callback) {
+    setTimeout(function () {
         console.log('f3');
         if (callback) callback();
     }, rand());
 }
 
-f1();
-f2();
-f3();
+f1(f1Callback);
 
+function f1Callback() {
+    f2(f2Callback);
+}
 
+function f2Callback() {
+    f3(f3Callback);
+}
+
+function f3Callback() {
+    console.log('Olá Mundo.')
+}
